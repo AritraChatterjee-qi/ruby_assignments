b716cf18e6d22959e527df1db70494062a901585
AritraChatterjee-qi <aritra.chatterjee@quantuminventions.com>
2024-02-01 10:31:39 +0530

    improved code style

--- Assignment_02.rb
+++ Assignment_02.rb
@@ -1,13 +1,15 @@
 # Write a program to convert { a: 12, b: 23, c: 34 } to { 12 => "a", 23 => "b", 34 => "c" }
 
-# returns a new Hash Obj
-def swap_key_val_01(my_hash) 
+# This function takes an hash as argument and returns a new hash object. <Symbol:Integer> to <Integer:String>
+def swap_key_val_one(my_hash) 
   is_hash = my_hash.kind_of?(Hash)
+
   unless is_hash
     return "ERROR! Fn input must be an array"
   end
 
   res_hash = Hash.new
+
   my_hash.each do |key, val|
     res_hash[val] = key.to_s
   end
@@ -15,14 +17,14 @@ def swap_key_val_01(my_hash)
   res_hash
 end
 
-# returns the old Hash Obj
-def swap_key_val_02(my_hash) 
+# This function takes an hash as argument. Modify and returns the same hash object. <Symbol:Integer> to <Integer:String>
+def swap_key_val_two(my_hash) 
   is_hash = my_hash.kind_of?(Hash)
+  
   unless is_hash
     return "ERROR! Fn input must be an array"
   end
 
-  #p my_hash.to_a
   my_hash.to_a.each do |key_val|
     my_hash[key_val[1]] = key_val[0].to_s
     my_hash.delete(key_val[0])
@@ -31,32 +33,11 @@ def swap_key_val_02(my_hash)
   my_hash
 end
 
-# returns a new Hash Obj
-# def swap_key_val_03(my_hash)
-#   is_hash = my_hash.kind_of?(Hash)
-#   unless is_hash
-#     return "ERROR! Fn input must be an array"
-#   end
-#   my_hash.invert
-# end
-
-
-
-# my_hash_00 = { "a" => 12, "b" => 23, "c" => 34 }
-my_hash_00 = { a: 12, b: 23, c: 34 }
-puts "Input Hash: #{my_hash_00} with obj_id: #{my_hash_00.object_id}"
-
+in_hash = { a: 12, b: 23, c: 34 }
+puts "Input Hash: #{in_hash} with obj_id: #{in_hash.object_id}"
 puts ""
-
-my_hash_01 = swap_key_val_01(my_hash_00)
-puts "Output_01: Hash: #{my_hash_01} with obj_id: #{my_hash_01.object_id}"
-
+out_hash_one = swap_key_val_one(in_hash)
+puts "Output_01: Hash: #{out_hash_one} with obj_id: #{out_hash_one.object_id}"
 puts ""
-
-# my_hash_03 = swap_key_val_03(my_hash_00)
-# puts "Output_03: Hash: #{my_hash_03} with obj_id: #{my_hash_03.object_id}"
-
-puts ""
-
-my_hash_02 = swap_key_val_02(my_hash_00)
-puts "Output_02: Hash: #{my_hash_02} with obj_id: #{my_hash_02.object_id}"
+out_hash_two = swap_key_val_two(in_hash)
+puts "Output_02: Hash: #{out_hash_two} with obj_id: #{out_hash_two.object_id}"
