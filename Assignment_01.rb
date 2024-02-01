6929441d98f1d0ea204b31ee4a1d753be125b216
AritraChatterjee-qi <aritra.chatterjee@quantuminventions.com>
2024-02-01 11:15:37 +0530

    function name changed, local variable, semicolon, extra lines removed

--- Assignment_01.rb
+++ Assignment_01.rb
@@ -1,38 +1,18 @@
 # This fuction takes an integer array as an argument and returns the sum of all the element.
-def arraySum(arr)
-  is_array = arr.kind_of?(Array)
-  
-  if !is_array
+def array_sum(arr)
+  unless arr.kind_of?(Array)
     return "ERROR! Function input must be an array"
   end
 
-  sum = 0;
+  sum = 0
 
-  # using for loop to calculate the sum of all elements in the array
-  #for i in 0...arr.length
-  #  sum += arr[i];
-  #end
-
-  # fusing or in loop to calculate the sum of all elements in the array
-  #for ele in arr
-  #    sum += ele;
-  #end
-
-  # using for each loop to calculate the sum of all elements in the array
   arr.each do |ele|
-     sum += ele;
+     sum += ele
   end
 
-  # using while loop to calculate the sum of all elements in the array
-  #idx = 0;
-  #while idx < arr.length
-  #  sum += arr[idx];
-  #  idx += 1;
-  #end
-
-    sum;
+  sum
 end
 
-arr = [1,2,3,4,5,6,7,8,9,10,11];
-result = arraySum(arr);
-puts "Sum of input #{arr} is #{result}";
+arr = [1,2,3,4,5,6,7,8,9,10]
+result = array_sum(arr)
+puts "Sum of input #{arr} is #{result}"
