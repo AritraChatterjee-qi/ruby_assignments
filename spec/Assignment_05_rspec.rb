1aa78d3bd89467d0b1d6b6ded0d0bdde138743e2
AritraChatterjee-qi <aritra.chatterjee@quantuminventions.com>
2024-02-06 12:17:10 +0530

    code structure improved

--- spec/Assignment_05_rspec.rb
+++ spec/Assignment_05_rspec.rb
@@ -1,48 +1,61 @@
 require_relative '../Assignment_05.rb'
 
 RSpec.describe Task do 
-    context "When testing the Task class" do 
-      
-      it "Testing total_time_in_minute method of the class" do 
-         task = Task.new( 10, "task10", [["2023-01-01 17:30:00", "2023-01-01 18:30:00"]] )
-         output = task.total_time_in_minute
-         expect(output).to eq 60
-      end
-      it "Testing occupation_percecntage_day_wise method of the class" do 
-         task = Task.new( 10, "task10", [["2023-01-01 17:30:00", "2023-01-01 18:30:00"]] )
-         output = task.occupation_percecntage_day_wise(task.total_time_in_minute)
-         expect(output).to eq 4.17
-      end
-      it "Testing valid? - test_01 of the class" do 
-         task = Task.new( 10, "task10", [["2023-01-01 17:30:00", "2023-01-01 18:30:00"]] )
-         output = task.valid?
-         expect(output).to be true
-      end
-      it "Testing valid? - test_01 of the class" do 
-         task = Task.new( 10, "task10", [["2023-01-01 19:30:00", "2023-01-01 18:30:00"]] )
-         output = task.valid?
-         expect(output).to be false
-      end
-      
-   end
+  describe "#total_time_in_minute" do
+    it "Calculate the total tine for a particular class" do 
+      task = Task.new( 10, "task10", [["2023-01-01 17:30:00", "2023-01-01 18:30:00"]] )
+      output = task.total_time_in_minute
+      expect(output).to eq 60
+    end
+  end
+
+  describe "#occupation_percecntage_day_wise" do
+    it "Calculate the total tine for a particular class as percentage of a day" do 
+      task = Task.new( 10, "task10", [["2023-01-01 17:30:00", "2023-01-01 18:30:00"]] )
+      output = task.occupation_percecntage_day_wise(task.total_time_in_minute)
+      expect(output).to eq 4.17
+    end
+  end
+
+  describe "#valid?" do
+     context "When the input is valid" do
+        it "Tests the input and checks if it is a valid input for not" do
+          task = Task.new( 10, "task10", [["2023-01-01 17:30:00", "2023-01-01 18:30:00"]] )
+          output = task.valid?
+          expect(output).to be true
+        end
+     end
+
+     context "When the input is invalid" do
+        it "Tests the input and checks if it is a valid input for not" do
+           task = Task.new( 10, "task10", [["2023-01-01 19:30:00", "2023-01-01 18:30:00"]] )
+           output = task.valid?
+           expect(output).to be false
+        end
+     end
+  end
 end
 
 RSpec.describe TaskHandler do 
-  context "When testing the Task Handler class" do 
-     
-     it "Testing process_input -- 01 method of the class" do 
-        task_handler = TaskHandler.new
-        task_handler.add_one_task({ id: 9, name: "task9", time_logs: [["2023-01-01 18:30:00", "2023-01-01 18:30:00"]] })
-        output = task_handler.process_input
-        desired_output = { categories: [], total_time_in_min: [], occupation_percecntage: [] }
-        expect(output).to eq desired_output
+  describe "#process_input" do
+     context "When the input is valid" do
+        it "proccess the input and generates the empty output" do
+           task_handler = TaskHandler.new
+           task_handler.add_one_task({ id: 9, name: "task9", time_logs: [["2023-01-01 18:30:00", "2023-01-01 18:30:00"]] })
+           output = task_handler.process_input
+           desired_output = { categories: [], total_time_in_min: [], occupation_percecntage: [] }
+           expect(output).to eq desired_output
+        end
      end
-     it "Testing process_input -- =2 method of the class" do 
-        task_handler = TaskHandler.new
-        task_handler.add_one_task({ id: 10, name: "task10", time_logs: [["2023-01-01 17:30:00", "2023-01-01 18:30:00"]] })
-        output = task_handler.process_input
-        desired_output = { categories: ["task10"], total_time_in_min: [60], occupation_percecntage: [4.17] }
-        expect(output).to eq desired_output
+
+     context "When the input is invalid" do
+        it "proccess the input and generates the output report" do
+           task_handler = TaskHandler.new
+           task_handler.add_one_task({ id: 10, name: "task10", time_logs: [["2023-01-01 17:30:00", "2023-01-01 18:30:00"]] })
+           output = task_handler.process_input
+           desired_output = { categories: ["task10"], total_time_in_min: [60], occupation_percecntage: [4.17] }
+           expect(output).to eq desired_output
+        end
      end
   end
 end
